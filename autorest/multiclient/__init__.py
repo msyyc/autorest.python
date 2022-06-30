588196ddae93f638ba7be36775fc496f4b54d14c
msyyc <70930885+msyyc@users.noreply.github.com>
2022-06-30 16:17:00 +0800

    optimize

--- autorest/multiclient/__init__.py
+++ autorest/multiclient/__init__.py
@@ -47,7 +47,7 @@ class MultiClientPlugin(Plugin):  # pylint: disable=abstract-method
         output_folder = filter_output_uri(str(self.options["outputFolderUri"]))
         version_folder = sorted(output_folder.glob("*/_version.py"))[0]
         module_version = "." + ".".join(version_folder.relative_to(output_folder).parts)
-        return module_version.replace(".py", "")
+        return module_version.strip(".py")
 
 
 class MultiClientPluginAutorest(MultiClientPlugin, PluginAutorest):
